78165c3e04bd66e89ea11f0350af70e4c2915b01
shabrina-septhyan <shabrina_nuraini_33rpl@student.smktelkom-mlg.sch.id>
2024-09-20 22:56:19 +0700

    Add files via upload

---
+++ DeretAritmatikaFor.java
@@ -0,0 +1,24 @@
+package Perulangan;
+
+import java.util.Scanner;
+
+public class DeretAritmatikaFor {
+    public static void main(String[]args)  {
+        int n, a, b, hasil;
+        Scanner s = new Scanner (System.in);
+        System.out.println("Masukan suku pertama: ");
+        a = s.nextInt();
+        System.out.println("Masukkan jumlah suku: ");
+        n = s.nextInt();
+        System.out.println("Masukkan beda: ");
+        b = s.nextInt();
+        
+        for (int i = 1; i <=n ; i++) {
+            hasil = a+(i-1)*b;
+            System.out.println("suku ke- "+i+"bernilai: " +hasil);
+        }
+}
+
+}
+    
+
