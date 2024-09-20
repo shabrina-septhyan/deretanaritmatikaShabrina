b6f04a1d11546d20cc4f17ee83d7c46c8edc9e70
shabrina-septhyan <shabrina_nuraini_33rpl@student.smktelkom-mlg.sch.id>
2024-09-20 22:55:42 +0700

    Add files via upload

---
+++ DeretAritmatikaWhile.java
@@ -0,0 +1,23 @@
+package Perulangan;
+
+import java.util.Scanner;
+
+public class DeretAritmatikaWhile {
+    public static void main(String[] args) {
+        int n, a, b, hasil, i = 1;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Masukkan suku pertama: ");
+        a = s.nextInt();
+        System.out.println("Masukkan suku jumlah suku");
+        n = s.nextInt();
+        System.out.println("Masukkan beda: ");
+        b = s.nextInt();
+
+        while (i<=n) {
+            hasil = a+(i-1)*b;
+            System.out.println("suku ke- "+i+"bernilai: " +hasil);
+            i++;
+}
+}
+}
+
